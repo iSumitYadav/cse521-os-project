aa8edb390ca2c6c5ab4f9cf8f3ab4057516d7a3f
Sumit Yadav <sumityadavcools@gmail.com>
2019-10-20 06:32:42 -0400

    P1 | AlarmClock | alarm-priority done

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -223,6 +223,16 @@ thread_block (void)
   intr_set_level (old_level);
 }
 
+// Though it's already present in src/tests/internal/list.c
+/* Returns true if value A is less than value B, false
+   otherwise. */
+static bool value_less_prio(const struct list_elem *a_, const struct list_elem *b_, void *aux){
+  const struct thread *a = list_entry (a_, struct thread, elem);
+  const struct thread *b = list_entry (b_, struct thread, elem);
+
+    return a->priority > b->priority;
+}
+
 /* Transitions a blocked thread T to the ready-to-run state.
    This is an error if T is not blocked.  (Use thread_yield() to
    make the running thread ready.)
@@ -240,7 +250,9 @@ thread_unblock (struct thread *t)
 
   old_level = intr_disable ();
   ASSERT (t->status == THREAD_BLOCKED);
-  list_push_back (&ready_list, &t->elem);
+
+  list_insert_ordered(&ready_list, &t->elem, value_less_prio, NULL);
+
   t->status = THREAD_READY;
   intr_set_level (old_level);
 }
@@ -310,8 +322,10 @@ thread_yield (void)
   ASSERT (!intr_context ());
 
   old_level = intr_disable ();
-  if (cur != idle_thread) 
-    list_push_back (&ready_list, &cur->elem);
+  if (cur != idle_thread){
+    list_insert_ordered(&ready_list, &cur->elem, value_less_prio, NULL);
+  }
+
   cur->status = THREAD_READY;
   schedule ();
   intr_set_level (old_level);
